3793c760a9dcd8233c6267ea78c9ef5a3945e049
Nyashes <118954257+GhostyBroski@users.noreply.github.com>
2025-04-24 01:28:46 -0700

    1st Team Activity

--- lesson_01/team/team01.py
+++ lesson_01/team/team01.py
@@ -2,7 +2,7 @@
 Course: CSE 351
 Lesson: L01 Team Activity
 File:   team.py
-Author: <Add name here>
+Author: Ash Jones
 Purpose: Find prime numbers
 
 Instructions:
@@ -44,6 +44,21 @@ def is_prime(n):
         i += 6
     return True
 
+def process_range(start, end, lock_prime, lock_processed):
+    global prime_count
+    global numbers_processed
+    for i in range(start, end):
+        if is_prime(i):
+            with lock_prime:
+                prime_count += 1
+            print(i, end=', ', flush=True)
+
+        with lock_processed:
+            numbers_processed += 1
+
+# Put this up here so I could access the variables needed and increase them when appropriate.
+# A function made more sense to me, from start to end-- For processing everything and the primes.
+
 
 def main():
     global prime_count                  # Required in order to use a global variable
@@ -54,17 +69,37 @@ def main():
 
     start = 10000000000
     range_count = 100000
-    numbers_processed = 0
-    for i in range(start, start + range_count):
-        numbers_processed += 1
-        if is_prime(i):
-            prime_count += 1
-            print(i, end=', ', flush=True)
-    print(flush=True)
+
+    # Locking the variables to avoid race conditions, so they work properly
+
+    lock_prime = threading.Lock()
+    lock_processed = threading.Lock()
+
+    number_threads = 10
+    threads = []
+    thread_range = range_count // number_threads
+
+    # Create threads and give each one a range to test, so that they can equally cover the entire range! (That makes the most sense to me)
+    for i in range(10):
+        thread_start = start + (thread_range * i)
+        thread_end = thread_start + thread_range
+        t = threading.Thread(target=process_range, args=(thread_start, thread_end, lock_prime, lock_processed))
+        threads.append(t)
+
+    # Start all threads off at the same time! (Kinda)
+    for t in threads:
+        t.start()
+
+    # From my memory this should cut off each thread
+    for t in threads:
+        t.join()
 
     # Should find 4306 primes
+    log.write('')
+    log.write(f'{range_count = }')
+    log.write(f'{number_threads = }')
     log.write(f'Numbers processed = {numbers_processed}')
-    log.write(f'Primes found      = {prime_count}')
+    log.write(f'Primes found = {prime_count}')
     log.stop_timer('Total time')
 
 
